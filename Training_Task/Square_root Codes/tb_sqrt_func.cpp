08915ae719c3720d5794ec7d86d7f92b5a177ee1
EmanMaqsood5 <emanmaqsood459@gmail.com>
2025-06-03 00:10:19 -0700

    Update tb_sqrt_func.cpp

--- Training_Task/Square_root Codes/tb_sqrt_func.cpp
+++ Training_Task/Square_root Codes/tb_sqrt_func.cpp
@@ -1,19 +1,22 @@
 #include "Vsqrt_func.h"
 #include "verilated.h"
 #include <iostream>
-#include <cmath>
 #include <iomanip>
+#include <cmath>
 
-int main() {
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
     Vsqrt_func* top = new Vsqrt_func;
 
-    uint32_t x = 10;
-    top->x = x;
+    
+    uint32_t input_fixed = 10 << 16;
+    top->x = input_fixed;
     top->eval();
 
+    double sqrt_result = static_cast<double>(top->result) / 65536.0;
     std::cout << std::fixed << std::setprecision(5);
-    std::cout << std::sqrt(static_cast<double>(x)) << std::endl;
+    std::cout << "sqrt(10.0) ≈ " << sqrt_result << std::endl;
 
     delete top;
     return 0;
-} -
+}
