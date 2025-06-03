50df9b29e274238699732fd0146468c39b52117b
EmanMaqsood5 <emanmaqsood459@gmail.com>
2025-06-03 00:11:58 -0700

    Update sqrt_func.sv

--- Training_Task/Square_root Codes/sqrt_func.sv
+++ Training_Task/Square_root Codes/sqrt_func.sv
@@ -1,53 +1,36 @@
-
 module sqrt_func (
     input  logic [31:0] x,            
     output logic [31:0] result    
 );
 
     logic [31:0] low, high, mid;   
-    logic [31:0] mid_squared, diff; 
+    logic [63:0] mid_squared; 
     integer i;
 
     always_comb begin
-        
         result = 32'd0;
         low = 32'd0;
-        high = 32'd0;  
+        high = x;
         mid = 32'd0;
-        mid_squared = 32'd0;
-        diff = 32'd0;
+        mid_squared = 64'd0;
 
-        // Handle simple cases
-        if (x == 0 || x == 1) begin
-            result = x; 
-        end else if (x > 1) begin
-            high = x;  
-            
-            low = 32'd0;
-            mid = (low + high) / 2; 
-            
-            
-            for (i = 0; i < 32; i++) begin  
-                mid_squared = mid * mid;
+        if (x == 32'd0 || x == 32'd1) begin
+            result = x;
+        end else begin
+            for (i = 0; i < 32; i++) begin
+                mid = (low + high) >> 1;
+                mid_squared = (mid * mid) >> 16; 
 
-                
-                if (mid_squared > high)
-                    diff = mid_squared - high;
-                else
-                    diff = high - mid_squared;
-
-                
-                if (diff <= 32'd1) begin 
+                if (mid_squared == 64'(x)) begin
                     result = mid;
                     break;
-                end else if (mid_squared < high) begin
+                end else if (mid_squared < 64'(x)) begin
                     low = mid;
                 end else begin
                     high = mid;
                 end
-                
-                
-                mid = (low + high) / 2;
+
+                result = mid; 
             end
         end
     end
